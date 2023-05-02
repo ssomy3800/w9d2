5ac3c2f3968653a197fd55d5bc4627fb69c65a4c
ssomy3800 <ssomy3800@gmail.com>
2023-05-02 11:02:14 -0700

    second commit

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,10 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import View from "./ttt-view.js";
+import Game from "../ttt_node/game.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const gameContainer = document.querySelector(".ttt");
+  const view = new View(game, gameContainer);
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,5 +1,6 @@
 class View {
   constructor(game, el) {
+    
   }
   
   setupBoard() {
