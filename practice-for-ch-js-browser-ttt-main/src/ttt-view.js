d17a94acaaadb31910c6f3670f7b1eedeb17a73c
ssomy3800 <ssomy3800@gmail.com>
2023-05-02 11:43:34 -0700

    third commit

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,19 +1,27 @@
 class View {
   constructor(game, el) {
-    
+    this.game = game;
+    this.el = el;
   }
-  
   setupBoard() {
-  }
-  
-  handleClick(e) {
+    let ul = document.createElement("ul");
+    let arr = [];
+    for (let i = 0; i < 9; i++) {
+      let x = Math.floor(i / 3);
+      let y = i % 3;
+      let listItem = document.createElement("li");
+      listItem.dataset.pos = `[${x},${y}]`;
+      arr.push(listItem);
+    }
+    ul.innerHTML = arr;
+    el.append(ul);
   }
 
-  makeMove(square) {
-  }
-  
-  handleGameOver() {
-  }
+  handleClick(e) {}
+
+  makeMove(square) {}
+
+  handleGameOver() {}
 }
 
 export default View;
